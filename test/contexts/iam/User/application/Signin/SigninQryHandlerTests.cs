e8c5f3eb60b6da995a6955bdca99b5cd4362e18c
Valentín Valverde <valenreta.developer@gmail.com>
2026-05-30 14:07:07 +0200

    test: refactor SigninQryHandlerTests for improved readability and structure

--- test/contexts/iam/User/application/Signin/SigninQryHandlerTests.cs
+++ test/contexts/iam/User/application/Signin/SigninQryHandlerTests.cs
@@ -1,28 +1,19 @@
 namespace Iam.User.application.Signin;
 
-
-
 public class SigninQryHandlerTests
 {
+    private readonly SigninQryHandler handler;
 
-  private readonly SigninQryHandler handler;
+    public SigninQryHandlerTests()
+    {
+        handler = new SigninQryHandler();
+    }
 
-  public SigninQryHandlerTests()
-  {
-    handler = new SigninQryHandler();
-  }
+    [Fact]
+    public async Task Handle_Should_Have_Handle_Method()
+    {
+        var query = new SigninQry("testuser", "password123");
 
-
-
-  [Fact]
-  public async Task Handle_Should_Have_Handle_Method()
-  {
-
-    var query = new SigninQry("testuser", "password123");
-
-    await handler.Handle(query);
-  }
-
-
-
+        await handler.Handle(query);
+    }
 }
