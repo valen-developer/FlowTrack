ee94ed52bc8ae344f76757270ff88e1db0c9799d
Valentín Valverde <valenreta.developer@gmail.com>
2026-05-31 04:02:22 +0200

    refactor: extract to class

---
+++ src/contexts/iam/Auth/Application/AuthTokenGenerator.cs
@@ -0,0 +1,58 @@
+using System.Collections.Immutable;
+using FlowTrack.Iam.Auth.Domain;
+using FlowTrack.Shared.Domain;
+using FlowTrack.Shared.Domain.Exception;
+using DomainUser = FlowTrack.Iam.User.Domain.User;
+
+namespace FlowTrack.Iam.Auth.Application;
+
+public sealed class AuthTokenGenerator(IEnvStore envStore, IJWTService jwtService)
+{
+    private const int DefaultAccessTokenExpireInMinutes = 60;
+    private const int DefaultRefreshTokenExpireInMinutes = 60 * 24 * 30;
+
+    public SigninSuccess Generate(DomainUser user)
+    {
+        var payload = GeneratePayload(user);
+        var accessTokenOptions = GenerateAccessTokenOptions();
+        var refreshTokenOptions = GenerateRefreshTokenOptions();
+
+        var accessToken = jwtService.Generate(payload, accessTokenOptions);
+        var refreshToken = jwtService.Generate(payload, refreshTokenOptions);
+
+        return new SigninSuccess(accessToken, refreshToken);
+    }
+
+    private JWTPayload GeneratePayload(DomainUser user)
+    {
+        return new JWTPayload(
+            new Dictionary<string, string> { { "id", user.Id.ToString() } }.ToImmutableDictionary()
+        );
+    }
+
+    private JWTOptions GenerateAccessTokenOptions()
+    {
+        var accessTokenSecret =
+            envStore.Get(IamEnvironmentKeysEnum.ACCESS_TOKEN_SECRET.ToString())
+            ?? throw new EnvVariableMissed(IamEnvironmentKeysEnum.ACCESS_TOKEN_SECRET.ToString());
+
+        var accessTokenExpireIn =
+            envStore.Get(IamEnvironmentKeysEnum.ACCESS_TOKEN_EXPIRE_MINUTES.ToString())
+            ?? DefaultAccessTokenExpireInMinutes.ToString();
+
+        return new JWTOptions(accessTokenSecret, int.Parse(accessTokenExpireIn));
+    }
+
+    private JWTOptions GenerateRefreshTokenOptions()
+    {
+        var refreshTokenSecret =
+            envStore.Get(IamEnvironmentKeysEnum.REFRESH_TOKEN_SECRET.ToString())
+            ?? throw new EnvVariableMissed(IamEnvironmentKeysEnum.REFRESH_TOKEN_SECRET.ToString());
+
+        var refreshTokenExpireIn =
+            envStore.Get(IamEnvironmentKeysEnum.REFRESH_TOKEN_EXPIRE_MINUTES.ToString())
+            ?? DefaultRefreshTokenExpireInMinutes.ToString();
+
+        return new JWTOptions(refreshTokenSecret, int.Parse(refreshTokenExpireIn));
+    }
+}
--- src/contexts/iam/Auth/Application/Signin/SigninQryHandler.cs
+++ src/contexts/iam/Auth/Application/Signin/SigninQryHandler.cs
@@ -9,52 +9,20 @@ namespace FlowTrack.Iam.Auth.Application.Signin;
 
 public sealed class SigninQryHandler(
     IUserRepository repository,
-    IBcrypt bcrypt,
-    IEnvStore envStore,
-    IJWTService jWTService
+    AuthTokenGenerator authTokenGenerator,
+    IBcrypt bcrypt
 ) : IQueryHandler<SigninQry, SigninSuccess>
 {
-    private readonly IUserRepository _repository = repository;
-    private readonly IBcrypt _bcrypt = bcrypt;
-    private readonly IEnvStore _envStore = envStore;
-
-    private readonly IJWTService _jWTService = jWTService;
-
     public async Task<SigninSuccess> Handle(SigninQry qry)
     {
-        var user = await _repository.FindByEmail(qry.Email) ?? throw new SigninFailed();
+        var user = await repository.FindByEmail(qry.Email) ?? throw new SigninFailed();
 
-        var isValidPassword = _bcrypt.Compare(qry.Password, user.Password);
+        var isValidPassword = bcrypt.Compare(qry.Password, user.Password);
         if (!isValidPassword)
             throw new SigninFailed();
 
-        var accessTokenSecret =
-            _envStore.Get(IamEnvironmentKeysEnum.ACCESS_TOKEN_SECRET.ToString())
-            ?? throw new EnvVariableMissed(IamEnvironmentKeysEnum.ACCESS_TOKEN_SECRET.ToString());
-        var accessTokenExpireIn =
-            _envStore.Get(IamEnvironmentKeysEnum.ACCESS_TOKEN_EXPIRE_MINUTES.ToString())
-            ?? 60.ToString();
+        var signinSuccess = authTokenGenerator.Generate(user);
 
-        var payload = new JWTPayload(
-            new Dictionary<string, string> { { "id", user.Id.ToString() } }.ToImmutableDictionary()
-        );
-
-        var accessTokenOptions = new JWTOptions(accessTokenSecret, int.Parse(accessTokenExpireIn));
-
-        var accessToken = _jWTService.Generate(payload, accessTokenOptions);
-
-        var refreshTokenSecret =
-            _envStore.Get(IamEnvironmentKeysEnum.REFRESH_TOKEN_SECRET.ToString())
-            ?? throw new EnvVariableMissed(IamEnvironmentKeysEnum.REFRESH_TOKEN_SECRET.ToString());
-        var refreshTokenExpireIn =
-            _envStore.Get(IamEnvironmentKeysEnum.REFRESH_TOKEN_EXPIRE_MINUTES.ToString())
-            ?? (60 * 24 * 30).ToString();
-
-        var refreshToken = _jWTService.Generate(
-            payload,
-            new JWTOptions(refreshTokenSecret ?? "", int.Parse(refreshTokenExpireIn))
-        );
-
-        return new SigninSuccess(accessToken, refreshToken);
+        return signinSuccess;
     }
 }
