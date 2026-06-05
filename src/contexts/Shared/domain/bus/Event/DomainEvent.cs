31ae5d7068eae9338543f11467241a10beebfd10
Valentín Valverde <valenreta.developer@gmail.com>
2026-06-06 00:10:43 +0200

    fix: remove unused Id property from DomainEvent record

--- src/contexts/Shared/domain/bus/Event/DomainEvent.cs
+++ src/contexts/Shared/domain/bus/Event/DomainEvent.cs
@@ -2,7 +2,6 @@ namespace FlowTrack.Shared.Domain;
 
 public abstract record DomainEvent(string code)
 {
-    public Guid Id { get; protected set; } = Guid.NewGuid();
     public DateTime OccurredOn { get; protected set; } = DateTime.UtcNow;
     public string Code { get; } = code;
 }
