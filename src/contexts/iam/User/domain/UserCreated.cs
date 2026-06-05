35bc94295b92b73628cfcd37144943bd39e10d57
Valentín Valverde <valenreta.developer@gmail.com>
2026-06-06 00:10:01 +0200

    fix: correct event type in UserCreated domain event

--- src/contexts/iam/User/domain/UserCreated.cs
+++ src/contexts/iam/User/domain/UserCreated.cs
@@ -2,4 +2,4 @@ using FlowTrack.Shared.Domain;
 
 namespace FlowTrack.Iam.Domain;
 
-public record UserCreated(Guid UserId, string Email, bool IsActive) : DomainEvent("code");
+public record UserCreated(Guid UserId, string Email, bool IsActive) : DomainEvent("user.created");
