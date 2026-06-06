7d447c34b793f579fe38a45b880a8e4a4ee8fa0c
Valentín Valverde <valenreta.developer@gmail.com>
2026-06-06 15:54:13 +0200

    refactor: remove useless test

--- test/apps/FlowtrackApi/Iam/Controllers/SignupController/SignupControllerE2E.cs
+++ test/apps/FlowtrackApi/Iam/Controllers/SignupController/SignupControllerE2E.cs
@@ -8,15 +8,6 @@ namespace FlowtrackApi.Iam;
 public class SignupControllerE2E(FlowtrackApiFixture fixture) : FlowtrackApiE2E(fixture)
 {
     [Fact]
-    public async Task Should_return_201_Created()
-    {
-        var request = new { Email = "validEmail@email.com", Password = "validPassword123" };
-        var response = await HttpClient.PostAsJsonAsync("/auth/signup", request);
-
-        Assert.Equal(HttpStatusCode.Created, response.StatusCode);
-    }
-
-    [Fact]
     public async Task Should_Save_User_In_DB()
     {
         UserDao userDao =
